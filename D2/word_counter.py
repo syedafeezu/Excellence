cdd51e87f5433d69804d83d67ab86b34a3fdcad3
syedafeezu <syedafeezu@gmail.com>
2025-12-09 20:30:37 +0530

    Day 2 : Tasks Completed

---
+++ D2/word_counter.py
@@ -0,0 +1,9 @@
+sentence = input("Enter a Sentence: ")
+words = sentence.lower().split()
+count = {}
+for word in words:
+    count[word] = count.get(word, 0)
+    count[word] += 1
+
+for i in count:
+    print(f"{i} : {count[i]}")

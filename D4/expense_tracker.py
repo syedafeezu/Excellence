1e86940635a251f5f8b926e552c393454ed0e9a9
SYED AFEEZU RAHEMAN A <syedafeezu@gmail.com>
2025-12-12 01:50:47 +0530

    create expense tracker feature

--- D4/expense_tracker.py
+++ D4/expense_tracker.py
@@ -1,9 +1,61 @@
 import json
+
 def load_data():
-    with open
-def save_data()
-def add_expense()
-def view_expense()
-def total_expense()
-def main()
+    with open('D4\expenses.json', 'r') as file:
+        exp = file.read()
+        return json.loads(exp)
     
+def save_data(data):
+    with open('D4\expenses.json', 'w') as file:
+        return json.dump(data,file,indent=4)
+
+def add_expense(data):
+    item = input("Enter Item Name : ")
+    try:
+        amt = int(input("Enter the Amount : "))
+        if amt<0:
+            raise ValueError
+    except ValueError:
+        print('Enter a Valid Amount')
+    else:
+        data.append({'Item': item,
+                     'Amount':amt})
+    
+def view_expense(data):
+    print(json.dumps(data, indent=1))
+
+def total_expense(data):
+    tot=0
+    for i in data:
+        tot+=i['Amount']
+    return tot
+
+def main():
+    c=0
+    while(c!=4):
+        print('====Expense Tracker====')
+        print('1. Add Expense')
+        print('2. View Expense')
+        print('3. Total Spent')
+        print('4. Exit')
+        
+        c=int(input('Enter your choice : '))
+        
+        if(c==1):
+            data=load_data()
+            add_expense(data)
+            save_data(data)
+        elif(c==2):
+            data=load_data()
+            print("--Your Expenses--")
+            view_expense(data)    
+        elif(c==3):
+            data=load_data()
+            print('Total Spent : ',total_expense(data))
+        elif(c==4):
+            print("Thank You !")
+            break
+        else:
+            print("Enter a Valid Choice !")
+            
+main()

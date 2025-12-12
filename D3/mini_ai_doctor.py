2287a850d1c37d2c5ad67778dd5aaa5ab2ee6d17
SYED AFEEZU RAHEMAN A <syedafeezu@gmail.com>
2025-12-13 01:07:46 +0530

    modified: D3/mini_ai_doctor.py

--- D3/mini_ai_doctor.py
+++ D3/mini_ai_doctor.py
@@ -29,10 +29,25 @@ def diagnose(symptom):
 
     for word,message in rules.items():
         if word in symptom:
-            print(message)
             recognized = True
+            return message
     if not recognized:
-        print("--Symptom not recognized. Please consult a healthcare professional.")
+        return "--Symptom not recognized. Please consult a healthcare professional."
     
+    return 0
+    # if "fever" or "headache" or "cold" or "pain" in symptom:
+#         if "fever" in symptom:
+#             print("--You might have a viral infection.")
+#         if "headache" in symptom:
+#             print("--Possible dehydration or stress.")
+#         if "cold" in symptom:
+#             print("--You may have a common cold.")
+#         if "pain" in symptom:
+#             print("--Pain detected, consult a doctor.")
+#     else:
+#         print("--Symptom not recognized. Please consult a healthcare professional.")
+#     return 0
+# Both are functionally equivalent and theyre Mine! :D
+
 symptom = input("Enter your symptom: ").lower()
 print(diagnose(symptom))
